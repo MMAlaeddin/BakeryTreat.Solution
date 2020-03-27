c05d5dbc229bece7441f6d3622e17bf776aef088
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-27 14:11:43 -0700

    add edit action and view for flavor

--- BakeryTreat/Controllers/FlavorsController.cs
+++ BakeryTreat/Controllers/FlavorsController.cs
@@ -56,5 +56,22 @@ namespace BakeryTreat.Controllers
         .FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatType");
+      return View(thisTreat);
+    }
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor, int TreatId)
+    {
+      if (TreatId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
