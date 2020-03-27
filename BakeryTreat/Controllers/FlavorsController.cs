ed401a16e7a085a94add71763f939f7d86c6b7a2
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-27 13:22:42 -0700

    add boiler plate for flavors controller set up

--- BakeryTreat/Controllers/FlavorsController.cs
+++ BakeryTreat/Controllers/FlavorsController.cs
@@ -8,3 +8,19 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
+
+namespace BakeryTreat.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> UserManager, BakeryTreatContext db)
+    {
+      _userManager = UserManager;
+      _db = db;
+    }
+    
+  }
+}
